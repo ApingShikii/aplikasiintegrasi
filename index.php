87aa12aae86cecbf00a047b08a0457b9861bb669
AlfinMHSPOLITALA <alfin23@mhs.politala.ac.id>
2024-11-12 14:46:02 +0800

    tesh push mohon maaf

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
     echo "Hello World!";
     echo "kelas 3B - IT Proyek";
     echo "Siti Rukmana";
+    echo "Rifani Muhammad";
     echo "Sistem Integrasi";
     echo "Muhammad Fajar Ramadhan";
-    echo "Alfin";
-    echo "supi";
-
 ?>
